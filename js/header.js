ef260dba199d7548d5dcbad0e22d0788275654b1
Fabricio Monteiro Dos Santos <105099322+LenorSantos@users.noreply.github.com>
2023-10-31 02:04:59 -0300

    ajuste de pos-carregamento

--- js/header.js
+++ js/header.js
@@ -1,21 +1,15 @@
 $('header').load("../pages/header.html");
 
-$(document).ready(function () {
-    window.onload = () => {
-        // faz o top começar na linha do header
-        $('main').css("margin-top", $('header').height());
-        // faz o top começar na linha do header
-    }
-
-    // faz o top começar na linha do header
+window.onload = () => {
     $('main').css("margin-top", $('header').height());
-    // faz o top começar na linha do header
+}
 
+$('.h-login, .logo').ready(function () {
     $(".logo").click(() => {
         window.location.href = '/';
     });
-    
-    $('.h-login').click(function(e){
+
+    $('.h-login').click(function (e) {
         $('.login-area').removeClass('d-none').addClass('d-flex');
         $('body').css("overflow", "hidden");
         $('.login-area').load("../pages/login.html");
